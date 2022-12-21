7eed8c537341f2763ac864ee6b47ffae2aae239e
Martin <mrtn.ritter@googlemail.com>
2022-12-21 17:10:14 +0100

    Changed behaviour OS switch

--- keyboards/keychron/q6/q6.c
+++ keyboards/keychron/q6/q6.c
@@ -32,7 +32,11 @@ bool dip_switch_update_kb(uint8_t index, bool active) {
         return false;
     }
     if (index == 0) {
-        default_layer_set(1UL << (active ? 2 : 0));
+        if (active) {
+            reset_keyboard();
+        } else {
+            default_layer_set(0);
+        }
     }
     return true;
 }

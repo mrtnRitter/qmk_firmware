fc22c22d9a0a94dccac54f30d95b264c2ff6bef3
Martin <mrtn.ritter@googlemail.com>
2023-02-12 10:14:16 +0100

    Typo update

--- keyboards/keychron/q6/iso_encoder/config.h
+++ keyboards/keychron/q6/iso_encoder/config.h
@@ -36,10 +36,10 @@
 #define ENCODER_RESOLUTION 4
 #define ENCODER_DEFAULT_POS 0x3
 
-/* Enable caps-lock LED*/
+/* Enable caps-lock LED */
 // #define CAPS_LOCK_LED_INDEX 60
 
-/* Enable NUM_LOCK_LED_INDEX */
+/* Enable num-lock LED */
 // #define NUM_LOCK_LED_INDEX 37
 
 /* activate NKRO by default */
